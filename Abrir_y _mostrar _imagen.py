8d9ce546cbb1d1a4327baabf0d6e5661402be1af
MiguelMike54 <66104861+MiguelMike54@users.noreply.github.com>
2020-06-25 23:57:32 -0500

    Comente la funcion "cv2.imshow" para no tener problemas en el server

--- Abrir_y _mostrar _imagen.py
+++ Abrir_y _mostrar _imagen.py
@@ -2,7 +2,7 @@ import cv2                                #Importando OpenCv
 
 imagen=cv2.imread('logopy.png')           #creando variable que almacenará la imagen, dentro del parentesis colocamos el mombre y extension de la imagen
                                           #En este caso se hace solo así ya que la imagen esta en la misma carpeta, si estuviera en otra carpeta se pone la ruta.
-cv2.imshow('Hola soy una imagen', imagen) #con esta función mostramos la imagen, los parametros son: el nombre de la ventana y la variable donde esta la imagen.                                
+#cv2.imshow('Hola soy una imagen', imagen) #con esta función mostramos la imagen, los parametros son: el nombre de la ventana y la variable donde esta la imagen.                                
 
 cv2.waitKey(0)                            #Esta funcion mostrará la imagen cierto tiempo o en este caso hasta que digitemos algo en el teclado.
 
